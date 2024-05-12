9626cf169656ff47bd68db36a63db5c280138f6c
Roman Wróblewski <wroblewski2003r@wp.pl>
2024-05-12 14:23:49 +0200

    suchy html

---
+++ Notatnik-app/src/components/Sidebar.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Sidebar: React.FC = () => {
+  return (
+    <>
+      <section>
+        <h1>Notatnik</h1>
+        <button> Dodaj Notatkę</button>
+        <div>
+          <h1>Kategorię:</h1>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Sidebar;
