db9ac43f7a452152a1478fc51d73eda82e4ec361
Roman Wróblewski <wroblewski2003r@wp.pl>
2024-05-12 14:26:25 +0200

    dodanie klas

--- Notatnik-app/src/components/Sidebar.tsx
+++ Notatnik-app/src/components/Sidebar.tsx
@@ -3,11 +3,11 @@ import React from "react";
 const Sidebar: React.FC = () => {
   return (
     <>
-      <section>
-        <h1>Notatnik</h1>
-        <button> Dodaj Notatkę</button>
-        <div>
-          <h1>Kategorię:</h1>
+      <section className="sidebar-section">
+        <h1 className="sidebar-heading">Notatnik</h1>
+        <button className="sidebar-add-button"> Dodaj Notatkę</button>
+        <div className="sidebar-categories-block">
+          <h1 className="sidebar-categories-heading">Kategorię:</h1>
         </div>
       </section>
     </>
