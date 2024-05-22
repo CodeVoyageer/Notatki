3a389c26c09d46bb1adf6146d96baa7ef2471ff3
Roman Wróblewski <wroblewski2003r@wp.pl>
2024-05-22 16:31:16 +0200

    dodanie styli

--- Notatnik-app/src/components/Sidebar.tsx
+++ Notatnik-app/src/components/Sidebar.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-
+import "../Scss/sidebar.scss";
 const Sidebar: React.FC = () => {
   return (
     <>
       <section className="sidebar-section">
         <h1 className="sidebar-heading">Notatnik</h1>
         <button className="sidebar-add-button"> Dodaj Notatkę</button>
-        <div className="sidebar-categories-block">
-          <h1 className="sidebar-categories-heading">Kategorię:</h1>
-        </div>
+        <div className="sidebar-categories-block"></div>
       </section>
     </>
   );
